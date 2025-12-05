cc1a3398e07e3bc41143877ab18a694805af58d4
maugrdev <maugrdev@hotmail.com>
2025-12-04 21:23:19 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -3,13 +3,13 @@
 // === CRÍTICO: ¡DEBEN SER LOS VALORES EXACTOS DE RENDER! ===
 // ==========================================================
 $host = 'dpg-d4ouedbe5dus73cjc0e0-a.oregon-postgres.render.com'; // **¡HOST COMPLETO!**
-$usuario = 'pizza_3j9z'; // **¡USUARIO CORRECTO DE POSTGRESQL!**
+$usuario = 'root'; // **¡USUARIO CORRECTO DE POSTGRESQL!**
 $password = 'ahFPq1Tu2Sj8QN9FgkYuS3fxAMmvtvwt'; 
 $dbname = 'pizza_3j9z'; 
 $port = 5432; 
 
 // --- CONEXIÓN USANDO FUNCIONES DE POSTGRESQL (pg_connect) ---
-$conn_string = "host={$host} port={$port} dbname={$dbname} user={$usuario} password={$password}";
+$conn_string = "host={$host} port={$port} dbname={$dbname} user={$usuario} password={$password} sslmode=require";
 $conexion = pg_connect($conn_string);
 
 if (!$conexion) {
